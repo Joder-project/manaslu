ef7b5baf136a15fb502393cb5b6a49add20b034b
joder <xiejiedun@mail.com>
2023-09-20 11:48:08 +0800

    jdk21

--- manaslu-spring/src/main/java/org/manaslu/cache/spring/SpringEntityProcessor.java
+++ manaslu-spring/src/main/java/org/manaslu/cache/spring/SpringEntityProcessor.java
@@ -17,7 +17,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @SupportedAnnotationTypes("org.manaslu.cache.spring.GenerateRepository")
-@SupportedSourceVersion(SourceVersion.RELEASE_19)
+@SupportedSourceVersion(SourceVersion.RELEASE_21)
 @AutoService(Processor.class)
 public class SpringEntityProcessor extends AbstractProcessor {
 
